045531ae0b70e5a3d988659156b2e3f95ebb4550
Antoine De Vuyst <fortyonepointsix@gmail.com>
2018-04-05 20:05:00 -0400

    updates to smart contract

--- RSVPToken.sol
+++ RSVPToken.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.15;
 
 import "./StandardToken.sol";
 
+//Safe math library to make sure calculations always go right
 library SafeMath {
   function mul(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a * b;
@@ -28,49 +29,60 @@ library SafeMath {
   }
 }
 
-contract RSVPToken is StandardToken {
-    
+contract RSVPToken {
+
     //create tokens that represent meetup tickets
-    //a way to redeem them, 
-    //notify organizer that token has been redeemed 
+    //a way to redeem them,
+    //notify organizer that token has been redeemed
     //a way to transfer tickets between holders
     //a way to initially sell tickets
-    
-    
-     string public name;                   
-    uint8 public decimals = 0;    
+
+
+     string public eventName;
+    uint8 public decimals = 0;
     uint UnsoldTickets;
+
+    //list of ticket holders
+    mapping (address => uint256) public holders;
+
+    //list of admitted event attendees
     mapping (address => bool) admitted;
-    
-    function RSVPToken(  uint256 _initialAmount,
-        string _tokenName) {
-        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
-        totalSupply = _initialAmount;                        // Update total supply
-        name = _tokenName;                                   // Set the name for display purposes
-        UnsoldTickets = _initialAmount;         
+
+    //set number of tickets and event name when you deploy the contract
+    function RSVPToken(uint _numTickets,
+        string _eventName) {
+        UnsoldTickets = _numTickets;                        // Update total supply
+        eventName = _eventName;                                   // Set the name for display purposes
     }
-    
+
+
+    //when someone sends ether to the contract, they get tickets! Leftover ether gets sent back.
     function () payable {
-        uint256 TicketsBought = SafeMath.div(msg.value,100000000000000000);
+        uint256 TicketsBought = SafeMath.div(msg.value,10**17);
         if (UnsoldTickets < TicketsBought) {throw;}
-        balances[msg.sender] += TicketsBought;
+        holders[msg.sender] += TicketsBought;
         UnsoldTickets -= TicketsBought;
-        msg.sender.transfer(SafeMath.sub(msg.value,(TicketsBought*100000000000000000)));
-        
+        msg.sender.transfer(SafeMath.sub(msg.value,(TicketsBought*10**17)));
+
     }
-    
+
+
+
+    //redeem a ticket to gain entry to the event.
     function redeem() {
-        if(balances[msg.sender] >= 1){
-            balances[msg.sender] -= 1;
+        if(holders[msg.sender] >= 1){
+            holders[msg.sender] -= 1;
             admitted[msg.sender] = true;
         } else {
             admitted[msg.sender] = false;
         }
     }
-    
+
+
+    //Check if a particular person's address has been admitted to the event
       function IsAdmitted(address _owner) constant returns (bool) {
         return admitted[_owner];
     }
-    
-    
+
+
 }
